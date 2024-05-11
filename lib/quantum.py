19fff395279851160c528c944795f01013296546
Will Thomas <163149425+cmwt@users.noreply.github.com>
2024-05-11 12:43:33 +0100

    Add files via upload
    adding a .py file that we can use to draw from for constants and functions for the quantum aspect of the application

---
+++ lib/quantum.py
@@ -0,0 +1,24 @@
+# In this file, all of the constants that are required
+# for the quantum calculations can be found alongside the
+# functions used to calculate corresponding properties.
+
+# Constants
+
+h = 6.626e-34 # Planck constant (Js)
+c = 2.997e8 # Speed of light (m/s)
+e = 1.602e-19 # Unit charge (C)
+k = 1.381e-23 # Boltzmann constant (J/K)
+N = 6.022e23 # Avogadro's constant (mol^-1)
+F = 96485 # Faraday constant (C mol^-1)
+R = 8.314 # Ideal gas constant (JK^-1mol^-1)
+
+def energy_calc():
+    # some statement to get value from user input
+    # user input / h = v
+    print(v) # can later apply this to print to an output
+    c/v = lam
+    print(lam) # can later apply this to print to an output
+    1/lam = wv
+    print(wv)
+    1/v = t
+    print(t)
