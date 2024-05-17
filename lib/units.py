c5edf5d80740aae9921f1727ba40433eee1c00ed
Will Thomas <163149425+cmwt@users.noreply.github.com>
2024-05-17 15:13:07 +0100

    Update units.py
    added a docstring comment at the top of the file.
    amended the lists and dictionaries to include a unit abbreviated for each unit. 
    added electron volts and hartree units to energies, to make the converter more applicable to chemistry and physics.

--- lib/units.py
+++ lib/units.py
@@ -1,12 +1,22 @@
+"""
+This file contains the list for the unit types dropdown menu along with
+all of the scale factors for the unit conversions in the unit converter tab
+of the GUI
+"""
+
 unit_types = ["Length", "Time", "Volume", "Pressure", "Energy"]
 
-length_units = {"Meters":1, "Centimeters":1e-2, "Millimeters":1e-3,
-                "Micrometers":1e-6, "Nanometers":1e-9,
-                "Picometers":1e-12, "Femtometers":1e-15,
-                "Attometers":1e-18, "Angstroms":1e-10}
-time_units = {"Seconds":1, "Milliseconds":1e-3, "Nanoseconds":1e-9, "Picoseconds":1e-12, "Femtoseconds":1e-15,
-              "Minutes":60, "Hours":3600}
-vol_units = {"Cubic Centimeters": 1, "Cubic Decimeters": 1e3, "Cubic Meters": 1e6,
-             "Millilitres": 1, "Centilitres": 10, "Litres": 1e3}
-pressure_units = {"Pa":1, "kPa":1e3, "Atm":101325, "Bar":1e5, "Torr":133.322, "PSI":6894.76}
-energy_units = {"Joule":1, "Kilojoules":1e3, "Kilocalories":4184} # kJ/mol, eV, Hartree
+length_units = {"Meters (m)":1, "Centimeters (cm)":1e-2, "Millimeters (mm)":1e-3,
+                "Micrometers (μm)":1e-6, "Nanometers (nm)":1e-9,
+                "Picometers (pm)":1e-12, "Femtometers (fm)":1e-15,
+                "Attometers (am)":1e-18, "Angstroms (Å)":1e-10}
+time_units = {"Seconds (s)":1, "Milliseconds (ms)":1e-3, "Nanoseconds (ns)":1e-9,
+              "Picoseconds (ps)":1e-12, "Femtoseconds (fs)":1e-15,
+              "Minutes (min)":60, "Hours (hr)":3600}
+vol_units = {"Cubic Centimeters (cm³)": 1, "Cubic Decimeters (dm³)": 1e3,
+             "Cubic Meters (m³)": 1e6,
+             "Millilitres (ml)": 1, "Centilitres (cl)": 10, "Litres (l)": 1e3}
+pressure_units = {"Pascals (Pa)":1, "Kilopascals (kPa)":1e3, "Atmospheres (Atm)":101325,
+                  "Bar":1e5, "Torr":133.322, "Pounds per square Inch (PSI)":6894.76}
+energy_units = {"Joule (J)":1, "Kilojoules (kJ)":1e3, "Kilocalories (kcal)":4184,
+                "Electron-volts (eV)":1.60218e-19, "Hartree (Eh)": 4.35974e-18}
