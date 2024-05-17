5ebbc6b862f33892cc03cfaa717998f6d23016f3
Oliver Rhodes <163131858+OliverZhengRhodes@users.noreply.github.com>
2024-05-17 17:17:16 +0100

    Update main_cli.py
    added the quantum calc functiona and exit function

--- main_cli.py
+++ main_cli.py
@@ -1,20 +1,21 @@
 from lib.units import *
 from lib.quantum import *
 
+
 def opening_options():
     print("Welcome User to the CLI Unit Converter\n\
 What unit type would you like to convert?")
     for i in range(len(unit_types)):
         print(f"{i+1}-{unit_types[i]}")
-    print(f"{i+2}-About")
+    print(f"{i+2}-Quantum Calculations\n{i+3}-About\n{i+4}-Exit")
 #this ensures that the user input is an integer and within the options
     while True:
         try:
             choice = int(input())
-            if 0 < choice < 7:
+            if 0 < choice < 9:
                 break
             else:
-                print("Choose a value between 1 and 6")
+                print("Choose a value between 1 and 8")
             
         except ValueError:
             print('You entered a non integer value, try again.')
@@ -34,8 +35,19 @@ def run_choice(x):
         unit = pressure_units
     elif x == 5:
         unit = energy_units
-    else:
+    elif x ==6:
+        property_type =  int(input("Please select a property type and input its corresponding value in SI units\
+\n1-Time(s)\n2-Frequency(Hz)\n3-Energy(J)\n4-Wavelength(m)\n5-Wavenumber(m^-1)"))
+        if 0 < property_type < 6: 
+            quantum_calculator(property_type)
+        else:
+            print("invalid entry")
+            run_choice(6)
+    elif x ==7:
         about()
+    else:
+#this ends the python script and returns the user to the directory
+        quit()
     print("These are the units which this Calculator can convert\nPick the starting unit below:")
     for i, unit_name in enumerate(unit, start=1):
         print(f"{i}-{unit_name}")
@@ -77,6 +89,25 @@ def run_choice(x):
 #    print(list(unit)[starting_unit-1])#has the unit name as a string
 #    print(list(unit)[end_unit-1])#has the unit name as a string
     conversion(starting_unit-1, end_unit-1,input_value)
+
+def quantum_calculator(x):
+    global unit
+    val = int(input("Enter the value"))
+    if x == 1:
+        quantum_calc(val, "Time (s)")
+    elif x == 2:
+        quantum_calc(val, "Frequency (Hz)")
+    elif x == 3:
+        quantum_calc(val, "Energy (J)")
+    elif x == 4:
+        quantum_calc(val, "Wavelength (m)")
+    elif x == 5:
+        quantum_calc(val, "Wavenumber (m^-1)")
+    
+    print("\nReturning to the menu")
+    opening_options()
+        
+    
 
 def conversion(a,b,c):
     scale_factor = unit[list(unit)[a]]/unit[list(unit)[b]]
